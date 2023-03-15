f4c3d9f3aeb420149e4630df75e13628739a1971
Bin <k1234988@gmail.com>
2023-03-15 10:21:20 +0800

    123

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/resume';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='rounded-3xl drop-shadow-md  max-w-4xl mx-auto '>
+      <HashRouter >
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </HashRouter>
     </div>
+  </>
   );
 }
 
---
+++ src/pages/resume.js
@@ -0,0 +1,99 @@
+
+
+export default () => {
+    return (
+        <div class="resume">
+            <div class="left">
+                <h1>施承鑌</h1>
+                <h4>業務 | 實習生 </h4>
+                <p>在永豐金、錠嵂保經、創科資訊擔任過實習生，擁有許多金融證照，如:證券商業務員、銀行內部控制與稽核、信託業務人員、壽險業務員等證照</p>
+                <div class="buttons">
+                    <a class="btn fill" target="_blank">作品集</a>
+                    <a class="btn" target="_blank">CV</a>
+                </div>
+                <div class="block">
+                    <p></p>
+                    <p></p>
+                    <div class="h4">Skills</div>
+                    <h2>專業技能</h2>
+                    <ul class="listSkill">
+                        <li>
+                            <div class="circle">
+                                <div class="p90"></div>
+                                <div class="p180"></div>
+                                <div class="p270"></div>
+                                <div class="p360"></div>
+                            </div>
+                            基礎語法(html/js)
+                        </li>
+                        <li>
+                            <div class="circle">
+                                <div class="p90"></div>
+                                <div class="p180"></div>
+                                <div class="p270"></div>
+                                <div class="p360"></div>
+
+                            </div>
+                            SQL
+                        </li>
+                    </ul>
+                </div>
+                <div class="block">
+                    <div class="h4">Other Skills</div>
+                    <h2>專長</h2>
+                    <ul class="listProgram">
+                        <li>部門溝通
+                            <div class="bar">
+                                <div class="value p100"></div>
+                            </div>
+                        </li>
+                        <li>問題解決
+                            <div class="bar">
+                                <div class="value p100"></div>
+                            </div>
+                        </li>
+
+                        <li>創意與執行力
+                            <div class="bar">
+                                <div class="value p100"></div>
+                                </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="right">
+                <div class="decoration"></div>
+                <div class="block">
+                    <div class="h4">Experiences</div>
+                    <h2>經歷</h2>
+                    <ul class="listExperience">
+                        <li class="year">2020.09永豐金
+                            <ul>
+                                <li>1.和客戶溝通聯繫，做客戶溝通橋梁。</li>
+                                <li>2.金融商品問題處理。</li>
+
+                            </ul>
+                        </li>
+                        <li class="year">2019.09錠嵂
+                            <ul>
+                                <li>保單條款說明</li>
+                                <li>保單健診</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="block">
+                    <div class="h4">Contact</div>
+                    <h2>聯絡方式</h2>
+                    <ul>
+                        <li>
+                            <a href="contact@memes.com" target="_blank">contact@memes.com</a>
+                        </li>
+                        <li>(+886) 987-987687</li>
+                        <li>Line ID：LOVE1233</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
